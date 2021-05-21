ea381d9c06a5ed35f67bede9f3621a41278a7445
luizpy <luizcn123@hotmail.com>
2021-05-21 00:01:19 -0300

    Update nos comentários

--- VisaoComp/Codigos/3_colorSpaceRGB.py
+++ VisaoComp/Codigos/3_colorSpaceRGB.py
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------
 Titulo: Trabalhando com imagens no espaço RGB                    -
 Compania: Universidade de Brasília                               -
-Autor: Unbetables        Update: Luiz Felipe  Date: 08/05        -    
+Autor: Unbetables        Update: Luiz Felipe  Date: 21/05        -    
 ------------------------------------------------------------------
 
 """
@@ -12,14 +12,18 @@ import matplotlib.pyplot as plt
 
 imagem = cv2.imread("VisaoComp/Imagens/lena.jpg")
 
-azul, verde, vermelho = cv2.split(imagem) # Geralmente, a biblioteca OpenCV trata o espaço de cor RGB (red, green, blue) como BGR (blue, green, red), invertendo a ordem dos canais. Por este motivo, quando utilizamos a função split , conforme demonstrado no código, declaramos as variáveis na sequência azul, verde, vermelho.
+# Geralmente, o OpenCV trata o espaço de cor RGB (red, green, blue) como
+# BGR (blue, green, red), invertendo a ordem dos canais. Por este motivo,
+# quando utilizamos a função split, conforme demonstrado abaixo no código,
+# declaramos as variáveis na sequência azul, verde, vermelho
+azul, verde, vermelho = cv2.split(imagem) 
 
 #Canais de cores
 cv2.imshow("Canal Azul", azul) 
 cv2.imshow("Canal Verde", verde)
 cv2.imshow("Canal Vermelho", vermelho)
 
-#Salvando	imagens	dos	canais	separados
+#Salvando as imagens dos canais separadamente
 cv2.imwrite("VisaoComp/Imagens/frutas-canal-vermelho.jpeg",	vermelho)
 cv2.imwrite("VisaoComp/Imagens/frutas-canal-verde.jpeg",	verde)
 cv2.imwrite("VisaoComp/Imagens/frutas-canal-azul.jpeg", azul)
@@ -27,10 +31,11 @@ cv2.imwrite("VisaoComp/Imagens/frutas-canal-azul.jpeg", azul)
 imagem = cv2.merge((azul,verde,vermelho))
 cv2.imshow("Imagem combinada", imagem)
 
-cv2.waitKey(0) # A função waitKey quando recebe como parâmetro o inteiro 0, espera o usuário digitar uma tecla para encerrar a exibição da figura 
+# A função waitKey quando recebe como parâmetro o inteiro 0, espera o usuário
+# digitar uma tecla para encerrar a exibição da figura
+cv2.waitKey(0) 
 
-cv2.destroyAllWindows() # Destroi todas as janelas abertas que ficaram
-
-# Podemos ver o tipo de img com o seguinte comando ja conhecido em python
+# Fecha corretamente todas as janelas
+cv2.destroyAllWindows() 
 
 
