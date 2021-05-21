d501be8362239c0a2ad17c36c2358080d3344b3e
luizpy <luizcn123@hotmail.com>
2021-05-21 00:08:16 -0300

    Update comentários e caminho da imagem

--- VisaoComp/Codigos/4_colorSpaceHSV.py
+++ VisaoComp/Codigos/4_colorSpaceHSV.py
@@ -1,51 +1,56 @@
 """
 ------------------------------------------------------------------
-Titulo: Trabalhando com imagens no espaço HSV                     -
-Compania: Universidade de Brasília                                 -
-Autor: Unbetables        Update: Luiz Felipe  Date: 08/05       -    
+Titulo: Trabalhando com imagens no espaço HSV                    -
+Compania: Universidade de Brasília                               -
+Autor: Unbetables        Update: Luiz Felipe  Date: 08/05        -    
 ------------------------------------------------------------------
 
-***********************Sistema de cores HSV***********************
+**********************Sistema de cores HSV***********************
 
 H(Hue)
 
-É a componente que define a cor. Pode ser vista fisicamente como o comprimento de onda dominante da value.
+É a componente que define a cor. Pode ser vista fisicamente como o com
+primento de onda dominante da value
 
 S(Saturation)
 
 É a pureza ou intensidade da cor. Quanto maior o valor da
-saturação, ais pura será a cor; quanto enor, ais próxima ao
-seu om e inza la erá epresentada.
+saturação, mais pura será a cor; quanto menor, mais próxima ao
+seu tom e cinza ela será representada
 
 V(Value)
 
 Este valor é referente ao brilho da cor, à luminosidade ou
-escala e laridade. uanto aior uminosidade, u alor este
-componente, mais próximo ao branco a cor será representada;
-quanto menor, mais próxima ao preto.
-
+escala e claridade. 
 """
 
 import cv2
 
-imagem = cv2.imread("VisaoComp/Imagens/lena.jpg")
+imagem = cv2.imread("Imagens/lena.jpg")
 
 imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
 
-hue, saturation, value = cv2.split(imagem) # Com a função split conseguimos separar os canais H, S e V. 
+# Com a função split conseguimos separar os canais H, S e V.
+hue, saturation, value = cv2.split(imagem)  
 
 #Canais de cores
 cv2.imshow("Canal hue", hue) 
 cv2.imshow("Canal saturation", saturation)
 cv2.imshow("Canal value", value)
 
-#Salvando magens os anais eparados
+#Salvando magens os canais separados
 cv2.imwrite("Imagens/lena-canal-hue.jpeg", hue)
 cv2.imwrite("Imagens/lena-canal-saturation.jpeg", saturation)
 cv2.imwrite("Imagens/lena-canal-azul.jpeg",  value)
 
-lenajunta = cv2.merge((hue,saturation,value)) # Junta os canais
-lenajunta = cv2.cvtColor(lenajunta, cv2.COLOR_HSV2BGR) # foi ecessário converter magem para o espaço RGB antes de exibi-la. É válido lembrar que nossos monitores  operam exibindo imagens representadas no spaço e cor GB , por este motivo, essa onversão é necessária.
+# Junta os canais
+lenajunta = cv2.merge((hue,saturation,value)) 
+
+ # Foi ecessário converter imagem para o espaço RGB antes de exibi-la.
+ # É válido lembrar que nossos monitores operam exibindo imagens repre
+ # sentadas no espaço e cor RGB , por este motivo, essa conversão se 
+ # torna sempre necessária.
+lenajunta = cv2.cvtColor(lenajunta, cv2.COLOR_HSV2BGR)
 
 cv2.imshow("Lena combinada HSV",lenajunta)
 cv2.waitKey(0)
