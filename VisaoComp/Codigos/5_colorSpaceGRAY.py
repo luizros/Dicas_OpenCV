8762635d4ba7b06bb6bac937969017221d4710ca
luizpy <luizcn123@hotmail.com>
2021-05-21 00:14:24 -0300

    Corrigindo comentários e caminho de imagem

--- VisaoComp/Codigos/5_colorSpaceGRAY.py
+++ VisaoComp/Codigos/5_colorSpaceGRAY.py
@@ -2,21 +2,30 @@
 ------------------------------------------------------------------
 Titulo: Trabalhando com imagens em tons de cinza                 -
 Compania: Universidade de Brasília                               -
-Autor: Unbetables        Update: Luiz Felipe  Date: 08/05c       -    
+Autor: Unbetables        Update: Luiz Felipe  Date: 20/05       -    
 ------------------------------------------------------------------
 
-É válido mencionar que a imagem em tons de cinza, após a conversão, é gerada a partir da soma ponderada dos canais de cor vermelho, verde e azul. Nessa soma, os canais vermelho e verde apresentam um peso (coeficiente de ponderação) maior quando comparado ao canal azul, justamente pelo fato do olho humano ser mais sensível a essas cores.
+Curiosidade
+
+É válido mencionar que a imagem em tons de cinza, após a conversão,
+é gerada a partir da soma ponderada dos canais de cor vermelho, ver
+de e azul. Nessa soma, os canais vermelho e verde apresentam um peso
+(coeficiente de ponderação) maior quando comparado ao canal azul, jus
+tamente pelo fato do olho humano ser mais sensível a essas cores.
 
 """
 import cv2
 
-imagem = cv2.imread("VisaoComp/Imagens/lena.jpg") # Ler imagem. Lembre de colocar o caminho correto da pasta!
+# Ler imagem. Lembre de colocar o caminho correto da pasta!
+imagem = cv2.imread("Imagens/lena.jpg") 
 
-imagem = cv2.cvtColor(imagem, cv2.COLOR_RGB2GRAY) # Converte de RGB para tons de cinza
+# Converte de RGB para tons de cinza
+imagem = cv2.cvtColor(imagem, cv2.COLOR_RGB2GRAY) 
 
-cv2.imshow("Lena cinza", imagem) # Mostra a imagem
+# Mostra a imagem
+cv2.imshow("Lena cinza", imagem) 
 
-cv2.waitKey(0) # Fecha a imagem após digitar qualquer tecla 
+cv2.waitKey(0) 
 
 cv2.destroyAllWindows() 
 
