5132295088530550a99cbfb19b3b2c721730e908
luizpy <luizcn123@hotmail.com>
2021-05-20 23:29:54 -0300

    Corrigindo comentários

--- VisaoComp/Codigos/14_VideoDetectorFace.py
+++ VisaoComp/Codigos/14_VideoDetectorFace.py
@@ -1,16 +1,18 @@
 import cv2
 
 captura = cv2.VideoCapture(0)
-detector = cv2.CascadeClassifier('VisaoComp/Codigos/haarcascade_frontalface_default.xml',)
+detector = cv2.CascadeClassifier('VisaoComp/Codigos/haarcascade_frontalface_default.xml')
+
 while True:
+    
     ret, imagem = captura.read()
     img = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-    deteccoes = detector.detectMultiScale(img)
+    deteccoes = detector.detectMultiScale(img, minSize=(200,200))
     for (x,y,l,h) in deteccoes:
-        cv2.rectangle(imagem, (x,y), (x+l, y+h), (0,255,0), 2) # Desenhando uma retngulo facilmente na imagem
+        cv2.rectangle(imagem, (x,y), (x+l, y+h), (255,0,0), 2) # Desenhando uma retngulo facilmente na imagem
         cv2.imshow('imagem',imagem)
         
-    if cv2.waitKey(1) & 0xFF == ord("q"): # Espera o usuário pressionar a letra 'q' do teclado
+    if cv2.waitKey(1) & 0xFF in [ord("q"), ord('Q')]: # Espera o usuário pressionar a letra 'q' do teclado
         break # quebra o loop infinito
     
 captura.release()
--- VisaoComp/Codigos/1_Introducao.py
+++ VisaoComp/Codigos/1_Introducao.py
@@ -13,23 +13,32 @@ import numpy as np
 
 # EXIBINDO IMAGENS NA TELA COM OPENCV E PYPLOT 
 
-img = cv2.imread('VisaoComp/Imagens/lena.jpg') # Este comando lê a imagem e retorna uma matriz de pixels em formato BGR
+# Este comando lê a imagem e retorna uma matriz de pixels em formato BGR
+img = cv2.imread('VisaoComp/Imagens/lena.jpg')
 
-cv2.imshow("Imagem", img) # A função imshow mostra a imagem na tela
+# A função imshow mostra a imagem na tela
+cv2.imshow("Imagem", img)
 
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) # Vamos exibir também a mesma imagem mas agora com o comando imshow(). Como a img está em BGR, precisamos colocar em em RGB para exibila na tela corretamente
+# Vamos exibir também a mesma imagem, mas agora com o comando imshow().
+# Como a img está em BGR, precisamos colocar ela em RGB para exibila na
+# tela corretamente
 
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+# Vamos exibis também utilizando o método show() do pyplot
 plt.show()
 
-cv2.waitKey(0) # A função waitKey quando recebe como parâmetro o inteiro 0, espera o usuário digitar uma tecla para encerrar a exibição da figura 
+# A função waitKey quando recebe como parâmetro o inteiro 0, espera o
+# usuário digitar uma tecla para encerrar a exibição da figura
+cv2.waitKey(0)
 
-cv2.destroyAllWindows() # Destroi todas as janelas abertas que ficaram
+# Destroi todas as janelas abertas que ficaram
+cv2.destroyAllWindows()
 
 # Podemos ver o tipo de img com o seguinte comando ja conhecido em python
+print(type(img)) # Verificamos que uma imagem é uma matriz de pixels
 
-print(type(img)) # Com esse comando veremos que a essa imagem é um vetor de bits
-
-# Agora basta usarmos acessarmos método shape de img para sabermos qual é a dimensoes da imagem. Vamos utilizar o método shape 
-
+# Agora basta usarmos acessarmos método shape de img para sabermos qual é
+# a dimensoes da imagem. Vamos utilizar o método shape
 dimensoes = img.shape
 print(dimensoes)
