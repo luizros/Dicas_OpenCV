0a434c380f62b749d07edf5bd3b9391774278fb2
luizpy <luizcn123@hotmail.com>
2021-05-21 00:29:05 -0300

    Inserindo comentários

--- VisaoComp/Codigos/8_equalizandoimagens.py
+++ VisaoComp/Codigos/8_equalizandoimagens.py
@@ -2,8 +2,14 @@
 ------------------------------------------------------------------
 Titulo: Equalizando imagens                                      -
 Compania: Universidade de Brasília                               -
-Autor: Unbetables        Update: Luiz Felipe  Date: 08/05        -    
+Autor: Unbetables        Update: Luiz Felipe  Date: 21/05        -    
 ------------------------------------------------------------------
+Equalizando imagens
+
+Digamos que você possui uma imagem em baixa qualidade e deseja melhorar
+sua vizualização. Um dos métodos mais utilzados para tratar esse tipo de
+problema é a equalização de imagens por histograms, que consiste em aumen
+tar o contraste da imagem. 
 
 """
 
@@ -11,7 +17,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as grafico
 
-imagemOriginal = cv2.imread("VisaoComp/Imagens/valvula.jpg", 0)
+imagemOriginal = cv2.imread("Imagens/valvula.jpg", 0)
 imagemEqualizada = cv2.equalizeHist(imagemOriginal)
 
 cv2.imshow("Imagem Original", imagemOriginal)
