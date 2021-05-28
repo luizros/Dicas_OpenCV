8195137838b00bde39f58fd3c980c7a9df84e7e4
Luiz <luizcn123@hotmail.com>
2021-05-27 23:10:26 -0300

    Primeiro commit

--- VisaoComp/Codigos/17_ajustedepespectiva.py
+++ VisaoComp/Codigos/17_ajustedepespectiva.py
@@ -15,3 +15,37 @@ ajusta a perspectiva de uma imagem tendo como referência uma matriz
 predefinida de pontos, gerada pela função getPerspectiveTransform.
 """
 
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+imagem = cv2.imread("VisaoComp/Imagens/sudoku.jpg")
+
+#use o imshow do matplotlib para saber os pontos de interesse
+#plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
+
+# nº ---------local---------   x   y
+# 1º ponto(superior esquerdo) 167 214
+# 2º ponto(inferior esquerdo) 109 810
+# 3º ponto(superior direito)  810 183
+# 4º ponto(inferior direito)  879 782
+
+
+pontosIniciais = np.float32(
+[[167,214], [810,183], [109,810], [879,782]]
+)
+pontosFinais = np.float32(
+[[0,0], [700,0], [0,700], [700,700]]
+)
+matriz = cv2.getPerspectiveTransform(
+pontosIniciais, pontosFinais
+)
+Imagemnova = cv2.warpPerspective(
+imagem, matriz, (700, 700)
+)
+cv2.imshow("Imagem Original", imagem)
+cv2.imshow("Imagem Nova", Imagemnova)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+plt.show()
+
