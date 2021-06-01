03972e586e35a4b4d9b359d814c44a594b84dd1f
Luiz <luizcn123@hotmail.com>
2021-06-01 18:20:04 -0300

    Ajustando identacao

--- VisaoComp/Codigos/17_ajustedepespectiva.py
+++ VisaoComp/Codigos/17_ajustedepespectiva.py
@@ -24,6 +24,7 @@ imagem = cv2.imread("VisaoComp/Imagens/sudoku.jpg")
 #use o imshow do matplotlib para saber os pontos de interesse
 #plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
 
+
 # nº ---------local---------   x   y
 # 1º ponto(superior esquerdo) 167 214
 # 2º ponto(inferior esquerdo) 109 810
@@ -31,21 +32,19 @@ imagem = cv2.imread("VisaoComp/Imagens/sudoku.jpg")
 # 4º ponto(inferior direito)  879 782
 
 
-pontosIniciais = np.float32(
-[[167,214], [810,183], [109,810], [879,782]]
-)
-pontosFinais = np.float32(
-[[0,0], [700,0], [0,700], [700,700]]
-)
-matriz = cv2.getPerspectiveTransform(
-pontosIniciais, pontosFinais
-)
-Imagemnova = cv2.warpPerspective(
-imagem, matriz, (700, 700)
-)
+pontosIniciais = np.float32([[167,214], [810,183], [109,810], [879,782]])
+
+pontosFinais = np.float32([[0,0], [600,0], [0,600], [600,600]])
+
+matriz = cv2.getPerspectiveTransform(pontosIniciais, pontosFinais)
+
+Imagemnova = cv2.warpPerspective(imagem, matriz, (600, 600))
+
 cv2.imshow("Imagem Original", imagem)
 cv2.imshow("Imagem Nova", Imagemnova)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 plt.show()
 
